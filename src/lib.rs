6d8bc36361af11da52903f192094c99788cc0e76
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-13 22:56:12 -0700

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,15 @@
 use proc_macro::TokenStream;
 use proc_macro_rules::rules;
 use quote::quote_spanned;
+//use readme_code_extractor_core::types::Config;
+use core::str::FromStr;
+
+#[doc(hidden)]
+const _ASSERT_README_CODE_EXTRACTOR_VERSION: () = {
+    if !readme_code_extractor_core::is_exact_version(env!("CARGO_PKG_VERSION")) {
+        panic!("prudent-rs/readme-code-extractor-core is of different version than prudent-rs/readme-code-extractor. Please report this as an issue, along with both versions.");
+    }
+};
 
 #[proc_macro]
 pub fn black_box(input: TokenStream) -> TokenStream {
@@ -12,6 +21,9 @@ pub fn black_box(input: TokenStream) -> TokenStream {
             let span = file_path_literal.span();
             // @TODO:
             let _ = span.local_file();
+
+            let file_content = "content";
+            let _ts = TokenStream::from_str(file_content).unwrap();
 
             let s = "Hi";
             let mut q = quote_spanned! {span=>
