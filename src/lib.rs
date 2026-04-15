cf2c1bdde60dcdb7346bdc294352eb431ea78047
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-15 09:09:23 -0700

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,26 @@ pub fn all(input: TokenStream) -> TokenStream {
     .into()
 }
 
+// Invoked by `readme_code_extractor::all_by_file`.
+#[doc(hidden)]
+#[proc_macro]
+pub fn all_by_file(input: TokenStream) -> TokenStream {
+    rules!(input.into() => {
+        ( $config_toml_file_path:literal ) => {
+
+            let span = config_toml_file_path.span();
+            
+            let config_toml_file_path = config_toml_file_path.to_string();
+            // @TODO error handling (or not)
+            let config_toml_content = std::fs::read_to_string(config_toml_file_path).unwrap();
+
+            quote_spanned! {span=>
+                ::readme_code_extractor_proc::all!(#config_toml_content)
+            }
+        }
+    }).into()
+}
+
 #[proc_macro]
 pub fn nth(_input: TokenStream) -> TokenStream {
     todo!()
@@ -53,6 +73,9 @@ pub fn create_nth_extractor_macro(input: TokenStream) -> TokenStream {
 
             let span = config_toml_content.span();
             quote_spanned! {span=>
+                // We do HAVE TO `macro_export` it. Otherwise it can't be used in `#[doc = ... ]` or
+                // `#![doc = ... ]` (which are processed in a crate separate to the crate that
+                // called `all_by_file` macro).
                 #[macro_export]
                 macro_rules! #name_of_new_extractor_macro {
                     ($n:literal) => {
