c47729ebe6bf8529b404f1036d85721d81ce5c9f
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-15 22:24:21 -0700

    MSRV to 1.85.1; WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,42 @@
 #![doc = include_str!("../README.md")]
 
+use core::str::FromStr;
 use proc_macro::TokenStream;
 use proc_macro2::Literal;
 use proc_macro_rules::rules;
 use quote::{quote, quote_spanned};
-//use readme_code_extractor_lib::types::Config;
-use core::str::FromStr;
+use readme_code_extractor_lib::traits::Config;
 //use std::path::Path;
 
-#[doc(hidden)]
 const _ASSERT_README_CODE_EXTRACTOR_LIB_VERSION: () = {
     if !readme_code_extractor_lib::is_exact_version(env!("CARGO_PKG_VERSION")) {
         panic!(
-            r"prudent-rs/readme-code-extractor-lib is of different version than
-                 prudent-rs/readme-code-extractor-proc. Please report this as an issue, along with
-                 both versions."
+            r"prudent-rs/readme-code-extractor-lib is of different version than 
+              prudent-rs/readme-code-extractor-proc. Please report this as an issue, along with 
+              both versions."
         );
     }
 };
+
+#[doc(hidden)]
+fn string_literal_content(literal: &Literal) -> String {
+    // Initially it's enclosed by "...", r"...", r#"..."# etc.
+    let enclosed = literal.to_string();
+    if enclosed.len() < 2 {
+        panic!("Expecting a string literal, but received: {}", enclosed);
+    }
+    // ASCII is common for code scope-only configuration, so applying the initial size same as
+    // number of bytes.
+    let mut chars = Vec::with_capacity(enclosed.len());
+    chars.extend(enclosed.chars());
+
+    enclosed
+}
+
+#[doc(hidden)]
+fn get_config(config_toml_content: Literal) -> Box<dyn Config> {
+    todo!()
+}
 
 #[proc_macro]
 pub fn all(input: TokenStream) -> TokenStream {
@@ -45,6 +64,109 @@ pub fn all(input: TokenStream) -> TokenStream {
     .into()
 }
 
+/// Restriction: We support only config (toml) files that
+/// - have paths
+///   - specified as ordinary string literals `"..."`.
+///     - whose characters/content (content of the path) don't include a quote '"' and some other
+///       special character, including backslashes! (Its representation in an ordinary, non-raw Rust
+///       string literal "...." (excluding the enclosing quotes) must be the same as it gets printed
+///       (in common terminals/screens).)
+///   - or: raw strings - TODO
+///      - RAW strings ARE GOOD - NO ESCAPING!
+///      - Good for backslashes and paths on Windows.
+/// - are in UTF-8 (the config content).
+///
+/// Return content of the config (toml) file.
+fn load_config_toml_file(config_toml_file_relative_path: &Literal) -> String {
+    // There does exist
+    // https://docs.rs/proc-macro2/latest/proc_macro2/struct.Literal.html#method.str_value, but
+    // - it's unclear how to enable it (`procmacro2_semver_exempt` is NOT a feature); and
+    // - it works with `nightly` Rust toolchain only.
+    //
+    // Instead,
+    // - call `proc_macro2::Literal`'s
+    //   [`to_string()`](https://docs.rs/proc-macro2/latest/proc_macro2/struct.Literal.html#impl-ToString-for-T)
+    // - that returns `String`, whose **content** is enclosed within quotes `"` and any quotes (and
+    //   special characters) are escaped.
+    // - simply
+    //   - if the string literal starts with a quote '"', remove the leading and trailing quotation
+    //     marks (or, actually, slice it).
+    //   - if the string literal starts with `r", r#", r##", r###"`, remove that and the appropriate
+    //     trailing group `", "#, "xx, "xxx`.
+    //
+    // Hence a restriction mentioned in rustdoc of this function.
+    let config_toml_file_path_enclosed = config_toml_file_relative_path.to_string();
+
+    {
+        // assertions
+        let config_toml_file_path_enclosed_bytes = config_toml_file_path_enclosed.as_bytes();
+        assert!(
+            config_toml_file_path_enclosed_bytes[0] == b'"',
+            "Expecting file path {} to start with an enclosing quote \".",
+            config_toml_file_path_enclosed
+        );
+
+        assert!(
+            config_toml_file_path_enclosed_bytes[config_toml_file_path_enclosed_bytes.len() - 1]
+                == b'"',
+            "Expecting file path {} to end with an enclosing quote \".",
+            config_toml_file_path_enclosed
+        );
+    }
+
+    let config_toml_file_path =
+        &config_toml_file_path_enclosed[1..config_toml_file_path_enclosed.len() - 1];
+
+    // Validate that the file path is compliant, by reversing the process, and then
+    // comparing the original and the result `String`. We use
+    // `proc_macro2::string(...).to_string()`.
+
+    {
+        //assertions
+        let regenerated_file_path_literal = Literal::string(config_toml_file_path);
+        let regenerated_file_path_enclosed = regenerated_file_path_literal.to_string();
+        assert_eq!(
+            config_toml_file_path_enclosed, regenerated_file_path_enclosed,
+            r"Can't parse/handle the given config (toml) file path literal (string) {}. It was
+                handled as {}.",
+            config_toml_file_path_enclosed, regenerated_file_path_enclosed
+        );
+    }
+
+    let cfg_file_path = {
+        let invoker_file_path = config_toml_file_relative_path
+            .span()
+            .local_file()
+            .unwrap_or_else(|| {
+                panic!(
+                    r"Rust source file that invoked readme_code_extractor::all_by_file!
+                    macro for config (toml) file with relative path {} should have a known
+                    location.",
+                    config_toml_file_relative_path
+                )
+            });
+        let invoker_parent_dir = invoker_file_path.parent().unwrap_or_else(|| {
+            panic!(
+                r"Rust source file that invoked readme_code_extractor::all_by_file!
+                    macro for config (toml) file with relative path {} may exist, but we 
+                    can't get its parent directory.",
+                config_toml_file_relative_path
+            )
+        });
+        invoker_parent_dir.join(config_toml_file_path)
+    };
+
+    // Error handling is modelling https://doc.rust-lang.org/nightly/src/core/result.rs.html
+    // > `fn unwrap_failed`, which invokes `panic!("{msg}: {error:?}");`
+    std::fs::read_to_string(&cfg_file_path).unwrap_or_else(|e| {
+        let cfg_file_path = cfg_file_path.to_str().unwrap_or("");
+        panic!(
+            "Expecting a config (toml) file {}, but opening it failed: {:?}",
+            cfg_file_path, e
+        )
+    })
+}
+
 // Invoked by `readme_code_extractor::all_by_file`.
 #[doc(hidden)]
 #[proc_macro]
@@ -53,77 +175,7 @@ pub fn all_by_file(input: TokenStream) -> TokenStream {
         ( $config_toml_file_relative_path:literal ) => {
 
             let span = config_toml_file_relative_path.span();
-
-            // There does exist
-            // https://docs.rs/proc-macro2/latest/proc_macro2/struct.Literal.html#method.str_value,
-            // but
-            // - it's unclear how to enable it (`procmacro2_semver_exempt` is NOT a feature); and
-            // - it works with `nightly` Rust toolchain only.
-            //
-            // Instead,
-            // - we call `proc_macro2::Literal`'s
-            //   [`to_string()`](https://docs.rs/proc-macro2/latest/proc_macro2/struct.Literal.html#impl-ToString-for-T)
-            // - that returns `String`, whose **content** is enslosed within quotes `"` and any
-            //   quotes (and special characters) are escaped.
-            // - we simply remove the leading and trailing quotation mark `"` (or, actually, slice
-            //   it).
-            //
-            // Hence a restriction: We support only such paths to config (toml) files that don't
-            // include a quote '"' and maybe other special characters. (Its representation in an
-            // ordinary, non-raw Rust string literal "...." must be the same as it gets printed (in
-            // common terminals/screens).)
-            let config_toml_file_path_enclosed = config_toml_file_relative_path.to_string();
-
-            {// assertions
-                let config_toml_file_path_enclosed_bytes
-                    = config_toml_file_path_enclosed.as_bytes();
-                assert!( config_toml_file_path_enclosed_bytes[0]==b'"',
-                    "Expecting file path {} to start with an enclosing quote \".",
-                    config_toml_file_path_enclosed);
-
-                assert!( config_toml_file_path_enclosed_bytes[
-                            config_toml_file_path_enclosed_bytes.len()-1
-                        ]==b'"',
-                        "Expecting file path {} to end with an enclosing quote \".",
-                        config_toml_file_path_enclosed);
-            }
-
-            let config_toml_file_path = &config_toml_file_path_enclosed[
-                1..config_toml_file_path_enclosed.len()-1
-            ];
-
-            // Validate that the file path is compliant, by reversing the process, and then
-            // comparing the original and the result `String`. We use
-            // `proc_macro2::string(...).to_string()`.
-
-            let regenerated_file_path_literal = Literal::string(config_toml_file_path);
-            let regenerated_file_path_enclosed = regenerated_file_path_literal.to_string();
-            assert_eq!(config_toml_file_path_enclosed, regenerated_file_path_enclosed,
-                r"Can't parse/handle the given config (toml) file path literal (string) {}. It was
-                  handled as {}.", config_toml_file_path_enclosed, regenerated_file_path_enclosed);
-
-            let cfg_file_path = {
-                let invoker_file_path = span.local_file().unwrap_or_else(|| {
-                    panic!(r"Rust source file that invoked readme_code_extractor::all_by_file!
-                            macro for config (toml) file with relative path {} should have a known
-                            location.", config_toml_file_relative_path)
-                });
-                let invoker_parent_dir = invoker_file_path.parent().unwrap_or_else(|| {
-                    panic!(r"Rust source file that invoked readme_code_extractor::all_by_file!
-                            macro for config (toml) file with relative path {} may exist, but we 
-                            can't get its parent directory.",
-                            config_toml_file_relative_path)
-                });
-                invoker_parent_dir.join( config_toml_file_path )
-            };
-
-            // Error handling is modelling https://doc.rust-lang.org/nightly/src/core/result.rs.html
-            // > `fn unwrap_failed`, which invokes `panic!("{msg}: {error:?}");`
-            let config_toml_content = std::fs::read_to_string(&cfg_file_path).unwrap_or_else(|e| {
-                let cfg_file_path = cfg_file_path.to_str().unwrap_or("");
-                panic!("Expecting a config (toml) file {}, but opening it failed: {:?}",
-                    cfg_file_path, e)
-            });
+            let config_toml_content = load_config_toml_file(&config_toml_file_relative_path);
 
             quote_spanned! {span=>
                 ::readme_code_extractor_proc::all!(#config_toml_content)
