fb567a0240888ec459be013b336f5bc8d8dc0e9f
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-29 19:55:35 -0700

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -113,7 +113,6 @@ fn nth_impl(input: TokenStream) -> MacroStreamResult {
 
             let cfg_content_and_span = readme_code_extractor_lib::public::config_content_and_span(
                 &config_toml_content)?;
-            //let code_block_index = match index.to_string().parse::<usize>() {
             let code_block_index = ok_or_err!(
                 cfg_content_and_span.span(),
                 index.to_string().parse::<usize>(),
@@ -181,7 +180,6 @@ macro_rules! token_stream_from_str {
     ($span:expr, $input_string:expr, $err_intended_result_description:expr) => {
         ({
             let input_string = $input_string;
-            //let result = TokenStream::from_str(input_string);
             ok_or_err!(
                 $span,
                 TokenStream::from_str(input_string),
@@ -239,11 +237,6 @@ where
         };
     let ordinary_code_suffix = config.ordinary_code_suffix();
 
-    let config_generated_len_per_block = prefix_before_insert.len()
-        + max_insert_len
-        + after_insert.len()
-        + ordinary_code_suffix.len();
-
     let blocks = readme_extracted
         .non_preamble_blocks()
         .collect::<MacroResult<Vec<_>>>()?;
@@ -251,9 +244,6 @@ where
     // @TODO apply backtick suffixes like "ignore" or "norun"
     let mut code_blocks = Vec::with_capacity(blocks.len() / 2 + 1);
     code_blocks.extend(blocks.iter().filter_map(ReadmeBlock::code));
-    //panic!("code_blocks: {}", code_blocks.len());
-    //
-    //panic!("code_blocks[0]: {}", code_blocks[0].code());
 
     true_or_err!(
         readme_extracted.span(),
@@ -268,8 +258,16 @@ where
         .map(|b| b.code().len())
         .max()
         .unwrap_or(0);
-    // @TODO triple_backtick_suffix
-    let mut code = String::with_capacity(config_generated_len_per_block + max_code_block_len);
+
+    let mut generated_per_block = {
+        let config_generated_len_per_block = prefix_before_insert.len()
+            + max_insert_len
+            + after_insert.len()
+            + ordinary_code_suffix.len();
+        // @TODO triple_backtick_suffix
+
+        String::with_capacity(config_generated_len_per_block + max_code_block_len)
+    };
 
     // @TODO preamble etc.
     let total_code_blocks_len = code_blocks.iter().map(|b| b.code().len()).sum::<usize>();
@@ -279,7 +277,7 @@ where
         format!("const _: &str = ::std::include_str!(\"{markdown_file_local_path}\");\n");
 
     // We don't count the length of all inserts. Using the maximum is good enough.
-    let mut all_code = String::with_capacity(
+    let mut generated_all = String::with_capacity(
         code_to_load_markdown_file.len()
             + config.start_prefix().len()
             + total_code_blocks_len
@@ -287,8 +285,8 @@ where
             + config.final_suffix().len(),
     );
 
-    all_code.push_str(&code_to_load_markdown_file);
-    all_code.push_str(config.start_prefix());
+    generated_all.push_str(&code_to_load_markdown_file);
+    generated_all.push_str(config.start_prefix());
 
     for (code_block_idx, (&block, &insert)) in
         code_blocks.iter().zip(inserts_iter_or_cycle).enumerate()
@@ -296,72 +294,41 @@ where
         if !code_block_filter(&code_blocks, code_block_idx, block, insert)? {
             continue;
         }
-        code.clear();
+        generated_per_block.clear();
         // @TODO triple_backtick_suffix
-        code.push_str(prefix_before_insert);
+        generated_per_block.push_str(prefix_before_insert);
         // @TODO insert
-        code.push_str(insert);
-        code.push_str(after_insert);
+        generated_per_block.push_str(insert);
+        generated_per_block.push_str(after_insert);
 
         let block_code = block.code();
         // Verify that the pushed part is a well-formed Rust token stream, that is, all parens (..),
         // brackets [..] and braces {..} are "balanced", string and char literals are well enclosed
         // etc.
         let _ = token_stream_from_str!(readme_extracted.span(), block_code, "Code block");
-        code.push_str(block_code);
+        generated_per_block.push_str(block_code);
 
-        // Verify a well-formed Rust token stream.
-        code.push_str(ordinary_code_suffix);
+        // Verify that the total output is a well-formed Rust token stream.
+        generated_per_block.push_str(ordinary_code_suffix);
         let _ = token_stream_from_str!(
             readme_extracted.span(),
-            &code,
+            &generated_per_block,
             "Extended code block: Prefix, insert, after_insert, the original code and suffix."
         );
 
-        all_code.push_str(&code);
+        generated_all.push_str(&generated_per_block);
     }
-    all_code.push_str(config.final_suffix());
+    generated_all.push_str(config.final_suffix());
 
     // Verify a well-formed Rust token stream.
     let main_token_stream = token_stream_from_str!(
         readme_extracted.span(),
-        &all_code,
-        "All code blocks extended, and with start_prefix and final_suffix"
+        &generated_all,
+        "Whole output: start_prefix, all code blocks extended, and final_suffix."
     );
     prefix_stream.extend(core::iter::once(main_token_stream));
     Ok(prefix_stream)
 }
-
-/*
-/// This is like `readme_code_extractor::all_by_file``, except that `all_by_file` is a declarative
-/// macro (macro by example, defined by `macro_rules`). However, [create_nth_extractor_macro] can't
-/// be declarative. Why? Because it itself defines a new declarative macro which needs to have a
-/// capturing variable (parameter). Capturing variables (parameters) start with a dollar character,
-/// but if [create_nth_extractor_macro] itself were a declarative macro, it couldn't generate/return
-/// a dollar character.
-#[proc_macro]
-pub fn create_nth_extractor_macro(input: ProcTokenStream) -> ProcTokenStream {
-    rules!(input.into() => {
-        ( $name_of_new_extractor_macro:ident, $config_toml_content:literal ) => {
-
-            let span = config_toml_content.span();
-            quote_spanned! {span=>
-                // We do HAVE TO `macro_export` it. Otherwise it can't be used in `#[doc = ... ]` or
-                // `#![doc = ... ]` (which are processed in a crate separate to the crate that
-                // called `all_by_file` macro).
-                #[macro_export]
-                macro_rules! #name_of_new_extractor_macro {
-                    ($n:literal) => {
-                        ::readme_code_extractor_proc::nth!($n, #config_toml_content);
-                        let _ = $$a;
-                    };
-                }
-            }
-        }
-    })
-    .into()
-}
-*/
 
 #[doc(hidden)]
 #[proc_macro]
