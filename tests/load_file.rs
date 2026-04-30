6e6dca5d9b9499ac35fbef01017dda1bdfb7c597
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-29 18:07:30 -0700

    Removed obsolete tests

--- tests/load_file.rs
+++
@@ -1,6 +0,0 @@
-#[test]
-fn test_load_file() {
-    // rust-analyzer WILL show an error here. Ignore.
-    let file_content = readme_code_extractor_proc::test_load_file!("file_1.txt");
-    assert_eq!(file_content, "Hi from file_1.txt");
-}
